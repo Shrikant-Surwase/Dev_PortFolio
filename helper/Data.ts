8c31301c8aeb12a6170c6d946718a3b3ed81e90f
Shrikant-Surwase <surwaseshrikant1729@gmail.com>
2024-05-07 18:57:02 +0530

    progress.....

--- helper/Data.ts
+++
@@ -1,5 +0,0 @@
-export const users=[
-    {id:"1",name:"shrikant",email:"surea@gmail.com",password:"123"},
-    {id:"2",name:"anand",email:"anans@gmail.com",password:"456"},
-    {id:"1",name:"surwase",email:"surwase@gmail.com",password:"765"},
-]
