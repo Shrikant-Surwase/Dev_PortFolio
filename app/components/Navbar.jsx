5515c842c34b90f3ef7ddf6148fddff98235f874
Shrikant Anand Surwase <97949621+Shrikant-Surwase@users.noreply.github.com>
2024-08-11 11:11:36 +0530

    Update Navbar.jsx

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
        <nav className="bg-transparent">
        <div className="flex items-center justify-between py-5">
         <div className="flex flex-shrink-0 items-center">
-            <Link href="/" className="text-[#16f2b3] text-3xl font-bold"  >Shrikant Surwase cool</Link>
+            <Link href="/" className="text-[#16f2b3] text-3xl font-bold"  >Shrikant Surwase</Link>
         </div>
         <ul className="mt-4 flex h-screen max-h-0 w-full flex-col items-start text-sm opacity-0 md:mt-0 md:h-auto md:w-auto md:max-h-screen md:flex-row md:opacity-100 md:space-x-1 md:border-0 ">
         <li>
